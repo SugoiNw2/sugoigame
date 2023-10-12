319e18c444c641fc525aefeacf6a2e0b572a4443
Guilherme Lima Silva <46793570+GuilhermeLimaSilva411@users.noreply.github.com>
2023-10-12 19:42:30 -0300

    Update vipLoja.php
    teste

--- public/Sessoes/vipLoja.php
+++ public/Sessoes/vipLoja.php
@@ -153,7 +153,7 @@
             "pacote.png",
             "Furto",
             "Rouba 15% das berries da tripulção derrotada no pvp<br>Limite de 2 'FURTOS' por adversário<br>Obs.: Essa vantagem dura apenas 1 dias e só pode ser comprada a cada 15 dias.",
-            $userDetails->vip["furto_duracao"],
+            // $userDetails->vip["furto_duracao"],
             PRECO_GOLD_FURTO,
             "",
             "Vip/furto_comprar.php?tpo=gold",
