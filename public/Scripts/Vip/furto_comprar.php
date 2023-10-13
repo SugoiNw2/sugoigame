9996ce869929810f918e382674e532d74be29ffa
Guilherme Lima Silva <46793570+GuilhermeLimaSilva411@users.noreply.github.com>
2023-10-13 20:24:46 -0300

    Update furto_comprar.php
    Funcionando

--- public/Scripts/Vip/furto_comprar.php
+++ public/Scripts/Vip/furto_comprar.php
@@ -1,5 +1,5 @@
 <?php
-   require "../../includes/concectdb.php";
+   require "../../includes/conectdb.php";
    include "../../includes/verifica_login.php";
 
    if (!$conect ){
@@ -15,14 +15,19 @@
    $tempo2     = $tempoBase2 + 15 * 24 * 60 * 60;
    
    // Verifica o limite do furto no banco de dados
-   $connection->run("SELECT furto_limite FROM tb_vip WHERE id = ?",
-      "i", $userDetails->tripulacao["id"]);
-   $result = $connection->fetch();
+   $query = $connection->prepare("SELECT furto_limite FROM tb_vip WHERE id = ?");
+   $query->bind_param("i", $userDetails->tripulacao["id"]);
+   $query->execute();
+   $result = $query->get_result()->fetch_assoc();
 
-   if ($result['furto_limtite'] === 0) {
+
+   if ($result['furto_limite'] === 0) {
       // permitir compra
-      $conncetion->run("UPDATE tb_vip SET furto = 1, furto_limte = ?, furto_duracao = ? WHERE id = ?",
-         "iii", array($tempo, $tempo2, $userDetails->tripulacao["id"]));
+      // Desconta o ouro 
+      $userDetails->reduz_gold(PRECO_GOLD_FURTO, "furto");
+
+      $connection->run("UPDATE tb_vip SET furto = 1, furto_limite = ?, furto_duracao = ? WHERE id = ?",
+         "iii", array($tempo2, $tempo, $userDetails->tripulacao["id"]));
       echo ("Você adquiriu o Furto!");
    } else {
       $tempoRestante = $tempoBase2 - time ();
@@ -31,9 +36,6 @@
       $horasRestantes = floor($tempoRestante / (60 * 60));
       $tempoRestante %= 60 * 60;
       $minutosRestantes = floor($tempoRestente / 60);
-      echo ("Você já comprou o furto no últimos 15 dias, você podera comprar novamente em: $diasRestantes, $horasRestantes e $minutosRestantes");
+      echo ("Você já comprou o furto nos últimos 15 dias, você podera comprar novamente em $diasRestantes Dias e $horasRestantes horas");
    }
-   
-   $userDetails->reduz_gold(PRECO_GOLD_FURTO, "furto");
-   
 ?>
